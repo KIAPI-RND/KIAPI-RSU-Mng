63566c04e4ae6fb390c37e91c19f5868e3dd6a5d
wcseo <wcseo.kr@gmail.com>
2026-03-16 10:34:18 +0900

    (프로젝트 저장소 최신화) 설정 configure 주석 반영

--- src/main.cc
+++ src/main.cc
@@ -61,29 +61,29 @@ int load_config_file(const std::string &path){
 
     auto val = json.get({"connection"});
 
-    config.index = (*val)["index"].GetInt();
-    config.is_server = (*val)["is_server"].GetBool();
-    config.ip = (*val)["ip"].GetString();
-    config.port =  (*val)["port"].GetInt();
+    config.index = (*val)["index"].GetInt();          // RSU 정보 연계 소프트웨어 실행 Index 번호
+    config.is_server = (*val)["is_server"].GetBool(); // RSU 접속 포트 Binding
+    config.ip = (*val)["ip"].GetString();             // RSU 접속 IP
+    config.port = (*val)["port"].GetInt();            // RSU 접속 Port
 
-    val = json.get({"v2x","rtt"});
+    val = json.get({"v2x", "rtt"});
 
-    config.rtt_set = (*val)["set"].GetBool();
-    config.rtt_interval = (*val)["interval"].GetInt();
-    config.rtt_size = (*val)["size"].GetInt();
+    config.rtt_set = (*val)["set"].GetBool();          // RTT 활성 유무 플래그
+    config.rtt_interval = (*val)["interval"].GetInt(); // RTT 전송 주기
+    config.rtt_size = (*val)["size"].GetInt();         // RTT 메시지 크기
 
-    val = json.get({"v2x", "ftp"});
-    config.ftp.ip = (*val)["ip"].GetString();
-    config.ftp.port = (*val)["port"].GetInt();
-    config.ftp.id = (*val)["id"].GetString();
-    config.ftp.pw = (*val)["pw"].GetString();
+    val = json.get({"v2x", "ftp"});            // FTP 업데이트 정보
+    config.ftp.ip = (*val)["ip"].GetString();  // FTP 접속 IP
+    config.ftp.port = (*val)["port"].GetInt(); // FTP 접속 포트
+    config.ftp.id = (*val)["id"].GetString();  // FTP 접속 ID
+    config.ftp.pw = (*val)["pw"].GetString();  // FTP 접속 PW
 
-    val = json.get({"db"}); 
-    config.db.ip = (*val)["ip"].GetString();
-    config.db.port = (*val)["port"].GetInt();
-    config.db.id = (*val)["id"].GetString();
-    config.db.pw =(*val)["pw"].GetString();
-    config.db.name = (*val)["name"].GetString();
+    val = json.get({"db"});
+    config.db.ip = (*val)["ip"].GetString();     // DB 접속 IP
+    config.db.port = (*val)["port"].GetInt();    // DB 접속 포트
+    config.db.id = (*val)["id"].GetString();     // DB 접속 ID
+    config.db.pw = (*val)["pw"].GetString();     // DB 접속 PW
+    config.db.name = (*val)["name"].GetString(); // DB NAME
 
     val = json.get({"dev"});
  
@@ -95,22 +95,22 @@ int load_config_file(const std::string &path){
         for (auto it = array.begin(); it != array.end(); it++)
         {
             nr_v2x_dev_info_t dev;
-            dev.ip = (*it)["ip"].GetString();
-            dev.serial = (*it)["serial"].GetString();
-            dev.dev_type = (nr_v2x_dev_type_t)(*it)["type"].GetInt();
-            dev.name = (*it)["name"].GetString();
-            dev.dev_id = (*it)["devId"].GetInt();
-            dev.ver = (*it)["version"].GetInt();
-            dev.pos.latitude = (*it)["pos"].GetArray()[0].GetDouble();
-            dev.pos.longitude = (*it)["pos"].GetArray()[1].GetDouble();
-            dev.pos.altitude = (*it)["pos"].GetArray()[2].GetDouble();
+            dev.ip = (*it)["ip"].GetString();                           // RSU IP 주소
+            dev.serial = (*it)["serial"].GetString();                   // RSU 시리얼 번호  
+            dev.dev_type = (nr_v2x_dev_type_t)(*it)["type"].GetInt();   // RSU 타입 (자체 IP or 외산 IP )
+            dev.name = (*it)["name"].GetString();                       // RSU 장치 명
+            dev.dev_id = (*it)["devId"].GetInt();                       // RSU 관리 ID
+            dev.ver = (*it)["version"].GetInt();                        // RSU 버전 코드 
+            dev.pos.latitude = (*it)["pos"].GetArray()[0].GetDouble();  // RSU 설치 위치 (위도)
+            dev.pos.longitude = (*it)["pos"].GetArray()[1].GetDouble(); // RSU 설치 위치 (경도)
+            dev.pos.altitude = (*it)["pos"].GetArray()[2].GetDouble();  // RSU 설치 위치 (고도)
 
-            dev.rtt.set = (*it)["rtt"].GetArray()[0].GetInt() > 0;
+            dev.rtt.set = (*it)["rtt"].GetArray()[0].GetInt() > 0; // RTT 활성 유무 
             if (dev.rtt.set)
             {
-                dev.rtt.ext = (*it)["rtt"].GetArray()[1].GetInt();
-                dev.rtt.size = (*it)["rtt"].GetArray()[2].GetInt();
-                dev.rtt.interval = (*it)["rtt"].GetArray()[3].GetInt();
+                dev.rtt.ext = (*it)["rtt"].GetArray()[1].GetInt();  // RTT Extention 필드 활성 유무 
+                dev.rtt.size = (*it)["rtt"].GetArray()[2].GetInt();  // RTT 전송 사이즈 
+                dev.rtt.interval = (*it)["rtt"].GetArray()[3].GetInt();  // RTT 전송 주기 
             }
 
             log_print(1, "+ dev[%d] [%d] [%s/%s][%s] [%lf,%lf,%lf] ",
